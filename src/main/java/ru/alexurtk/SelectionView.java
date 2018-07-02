9447ded36ef12b29d92288f3de91a1e5f6251b00
Ткаченко Александр <tkachenko.ay@reliab.tech>
2018-07-02 11:12:24 +0300

    test1
    test2
    
    test3

--- src/main/java/ru/alexurtk/SelectionView.java
+++ src/main/java/ru/alexurtk/SelectionView.java
@@ -231,10 +231,20 @@ public class SelectionView implements Serializable {
         }
 
 
-
-
         return null;
 
+    }
+
+    public void test1(){
+        System.out.println("test1");
+    }
+
+    public void test2(){
+        System.out.println("test2");
+    }
+
+    public void test3(){
+        System.out.println("test3");
     }
 
 
