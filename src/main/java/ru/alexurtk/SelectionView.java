15c8b39b7d05e139af20e863561d60e6f105afb8
Ткаченко Александр <tkachenko.ay@reliab.tech>
2018-07-02 11:31:23 +0300

    test rebase commit

--- src/main/java/ru/alexurtk/SelectionView.java
+++ src/main/java/ru/alexurtk/SelectionView.java
@@ -235,17 +235,4 @@ public class SelectionView implements Serializable {
 
     }
 
-    public void test1(){
-        System.out.println("test1");
-    }
-
-    public void test2(){
-        System.out.println("test2");
-    }
-
-    public void test3(){
-        System.out.println("test3");
-    }
-
-
 }
